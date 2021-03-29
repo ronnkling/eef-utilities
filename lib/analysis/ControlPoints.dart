1d4c910bfc50bf4cd7ebfdeac75c152365d1d50e
Louis Lu <louisyulu@gmail.com>
2021-03-29 17:04:54 -0400

    add linear spline

--- lib/analysis/ControlPoints.dart
+++ lib/analysis/ControlPoints.dart
@@ -1,3 +1,4 @@
+import 'package:scidart/numdart.dart';
 import 'utils.dart';
 
 class ControlPoints {
@@ -73,5 +74,75 @@ class ControlPoints {
       for (int i = 0; i < m; ++i) gE.add(gs[indices[i]]);
     }
     return yE.length;
+  }
+
+  // only apply to quintic spline
+  void qAdjustASide() {
+    //        if (xE.length < 5
+    //                || xE[1] - xE[0] >= xE[2] - xE[1]
+    //                || qAbs(yE[1] - yE[0]) > 0.5 * qAbs(yE[2] - yE[1]))
+    //            return;
+    //        gE[0] = gE[1] - yE[0] * (xE[1] - xE[0]);
+
+    if (xE.length < 5 || xE[1] - xE[0] >= xE[2] - xE[1]) return;
+    double dg =
+        _extendIntegral(xE[2], xE[1], xE[0], yE[1], yE[0], gE[0] - gE[1]);
+    xE[0] = 2 * xE[1] - xE[2];
+    gE[0] = gE[1] + dg;
+  }
+
+  // only apply to quintic spline
+  void qAdjustZSide() {
+    int m = xE.length - 1;
+    //        if (xE.length < 5
+    //                || xE[m] - xE[m - 1] >= xE[m - 1] - xE[m - 2]
+    //                || qAbs(yE[m] - yE[m - 1]) > 0.5 * qAbs(yE[m - 1] - yE[m - 2]))
+    //            return;
+    //        gE[m] = gE[m - 1] + yE[m] * (xE[m] - xE[m - 1]);
+
+    if (xE.length < 5 || xE[m] - xE[m - 1] >= xE[m - 1] - xE[m - 2]) return;
+    double dg = _extendIntegral(
+        xE[m - 2], xE[m - 1], xE[m], yE[m - 1], yE[m], gE[m] - gE[m - 1]);
+    xE[m] = 2 * xE[m - 1] - xE[m - 2];
+    gE[m] = gE[m - 1] + dg;
+  }
+
+  // extend the end point to a point with first derivative 0
+  // quintic polynome at^5 + bt^4 + ct^3 + et
+  static double _extendIntegral(
+      double x1, double x2, double x3, double y2, double y3, double g3) {
+    double c = x3 - x2;
+    double c2 = c * c;
+    double c3 = c2 * c;
+    double c4 = c3 * c;
+    double c5 = c4 * c;
+
+    double d = x2 - x1;
+    double d2 = d * d;
+    double d3 = d2 * d;
+    double d4 = d3 * d;
+    double d5 = d4 * d;
+
+    var a = Array2d.fixed(3, 3);
+    var b = Array2d.fixed(3, 1);
+
+    a[0][0] = c5;
+    a[0][1] = c4;
+    a[0][2] = c3;
+    b[0][0] = g3 - c * y2;
+
+    a[1][0] = 5 * c4;
+    a[1][1] = 4 * c3;
+    a[1][2] = 3 * c2;
+    b[1][0] = y3 - y2;
+
+    a[2][0] = 20 * d3;
+    a[2][1] = 12 * d2;
+    a[2][2] = 6 * d;
+    b[2][0] = 0.0;
+
+    var x = matrixSolve(a, b);
+    double g4 = x[0][0] * d5 + x[1][0] * d4 + x[2][0] * d3 + y2 * d;
+    return g4;
   }
 }
---
+++ lib/spline/Linear.dart
@@ -0,0 +1,94 @@
+import 'package:tuple/tuple.dart';
+import 'Spline.dart';
+
+class Linear implements Spline {
+  int N;
+  List<double> X;
+  List<double> Y;
+  late List<double> C;
+
+  Linear(this.X, this.Y) : N = X.length {
+    C = List.filled(N, 0.0);
+    int m = N - 1;
+    for (int i = 0; i < m; ++i) C[i] = (Y[i + 1] - Y[i]) / (X[i + 1] - X[i]);
+    C[m] = C[m - 1];
+  }
+
+  Tuple2<double, int> value(double x, {int k = 0}) {
+    var t = findSegment(X, x, k: k);
+    return Tuple2(fn(k, t.item1), k);
+  }
+
+  Tuple2<double, int> derivative(double x, {int k = 0}) {
+    var t = findSegment(X, x, k: k);
+    return Tuple2(df1(k, t.item1), k);
+  }
+
+  Tuple2<double, int> derivative2(double x, {int k = 0}) {
+    return Tuple2(0.0, k);
+  }
+
+  Tuple2<double, int> derivative3(double x, {int k = 0}) {
+    return Tuple2(0.0, k);
+  }
+
+  List<double> values(List<double> xs) {
+    int m = xs.length;
+    var vs = List<double>.filled(m, 0.0);
+    int k = 0;
+    for (int i = 0; i < m; i++) {
+      var t = findSegment(X, xs[i], k: k);
+      k = t.item2;
+      vs[i] = fn(k, t.item1);
+    }
+    return vs;
+  }
+
+  List<double> derivatives(List<double> xs) {
+    int m = xs.length;
+    var vs = List<double>.filled(m, 0.0);
+    int k = 0;
+    for (int i = 0; i < m; i++) {
+      var t = findSegment(X, xs[i], k: k);
+      k = t.item2;
+      vs[i] = df1(k, t.item1);
+    }
+    return vs;
+  }
+
+  List<double> derivatives2(List<double> xs) {
+    return List<double>.filled(xs.length, 0.0);
+  }
+
+  List<double> derivatives3(List<double> xs) {
+    return List<double>.filled(xs.length, 0.0);
+  }
+
+  void outputFormula(StringSink out) {
+    out.writeln('# linear spline: s(t) = a + b*t;  t = x - x0');
+    out.writeln('# a,  b,  [x0, x1)');
+    int m = N - 1;
+    for (int i = 0; i < m; i++) {
+      out.writeln('${Y[i]}, ${C[i]}, [${X[i]}, ${X[i + 1]})');
+    }
+    out.writeln('# total lines = $m');
+  }
+
+  void outputDerivative(StringSink out) {
+    out.writeln('# step function: s(t) = b;  t = x - x0');
+    out.writeln('# b,  [x0, x1)');
+    int m = N - 1;
+    for (int i = 0; i < m; i++) {
+      out.writeln('${C[i]}, [${X[i]}, ${X[i + 1]})');
+    }
+    out.writeln('# total lines = $m');
+  }
+
+  double fn(int k, double t) {
+    return C[k] * t + Y[k];
+  }
+
+  double df1(int k, double t) {
+    return C[k];
+  }
+}
